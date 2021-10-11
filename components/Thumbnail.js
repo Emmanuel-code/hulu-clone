1633103938eb705075e0ab6c0c86c36a82f6acde
Emmanuel-code <b.emma.j37@gmail.com>
2021-10-11 02:48:27 +0000

    qweq

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -8,7 +8,7 @@ function Thumbnail({result}) {
     return (
         <div className='group p-2 transition duration-200 ease-in transform sm:hover:scale-90 hover:z-50 cursor-pointer '>
             <Image height={1080} width={1920} src='https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg'/>
-<Image src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} layout='responsive' height={1080} width={1920} alt='movie photo'/>
+<img src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} layout='responsive' height={1080} width={1920} alt='movie photo'/>
 <div className='p-2' >
     <p className='truncate max-w-md'>{result.overview}</p>
     <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold '>{result.title || result.original_name}</h2>
